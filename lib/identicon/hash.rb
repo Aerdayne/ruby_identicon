0c693bb09560b7c3a4629866f055bd555127527f
Aerdayne <daenskus@gmail.com>
2020-03-01 00:24:47 +0300

    Visualization algorithm tweak & fixes

--- lib/identicon/hash.rb
+++ lib/identicon/hash.rb
@@ -5,22 +5,34 @@ require 'digest'
 class Identicon
   module Hash
     def digest
-      parse_binary(Digest::MD5
-                     .hexdigest(username)
-                     .scan(/.{2}/)
-                     .map { |h| h.unpack('b8') }
-                     .flatten)
+      @hash = Digest::MD5.hexdigest(username).scan(/.{2}/)
+      map
     end
 
     private
 
-    def parse_binary(bytes)
+    def map
+      map = {}
+      @hash[0..-2].each_with_index do |hex_pair, index|
+        map[index / 5] ||= []
+        map[index / 5][index % 5] = calculate(hex_pair)
+      end
+      map[3] = map[1]
+      map[4] = map[0]
+      digested_hash(map)
+    end
+
+    def digested_hash(map)
       {
-        byte_red: bytes[0],
-        byte_green: bytes[1],
-        byte_blue: bytes[2],
-        bit_array: bytes[3..15].join('')
+        red: @hash[-1].to_i(16),
+        green: @hash[0].to_i(16),
+        blue: @hash[8].to_i(16),
+        map: map
       }
+    end
+
+    def calculate(hex_pair)
+      (hex_pair[0].to_i(16) ^ hex_pair[1].to_i(16)).to_s(2).bytes[-1].odd? ? 1 : 0
     end
   end
 end
--- lib/identicon/image.rb
+++ lib/identicon/image.rb
@@ -6,26 +6,24 @@ class Identicon
     PIXEL_BORDER = 14
     PIXEL_OFFSET = 10
 
-    attr_reader :icon, :image, :hash
-
-    def generate_image(hash)
-      @hash = hash
+    def generate_image(data)
+      @data = data
       @icon = ChunkyPNG::Image.new(128 - PIXEL_BORDER,
                                    128 - PIXEL_BORDER,
                                    ChunkyPNG::Color.rgb(255, 255, 255))
       @image = ChunkyPNG::Image.new(128,
                                     128,
                                     ChunkyPNG::Color.rgb(255, 255, 255))
-      build_icon(hash)
+      build_icon
       @image.compose!(@icon, PIXEL_OFFSET, PIXEL_OFFSET)
       @image.save(path)
     end
 
     private
 
-    def build_icon(hash)
-      map(hash).each_key do |col_id|
-        col_values = @map[col_id]
+    def build_icon
+      @data[:map].each_key do |col_id|
+        col_values = digest[:map][col_id]
         col_values.each_with_index do |value, line_id|
           next if value.zero?
 
@@ -41,27 +39,14 @@ class Identicon
         (col_id.to_i + 1) * PIXEL_RATIO,
         (line_id + 1) * PIXEL_RATIO,
         ChunkyPNG::Color::TRANSPARENT,
-        color(hash)
+        color
       )
     end
 
-    def color(hash)
-      @color ||= ChunkyPNG::Color.rgb(hash[:byte_red].to_i(2), 
-                                      hash[:byte_green].to_i(2), 
-                                      hash[:byte_blue].to_i(2))
-    end
-
-    def map(hash)
-      array = hash[:bit_array].scan(/.{6}/)[0..14].map do |s|
-        s.to_i(2).even? ? 1 : 0
-      end
-      @map = {
-        '0' => array[0..4],
-        '1' => array[5..9],
-        '2' => array[10..14],
-        '3' => array[5..9],
-        '4' => array[0..4]
-      }
+    def color
+      ChunkyPNG::Color.rgb(@data[:red],
+                           @data[:green],
+                           @data[:blue])
     end
   end
 end
